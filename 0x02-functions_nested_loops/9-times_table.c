fbc6965540ac121a6d193c97bdfc409b31eae3d0
hudaelmahdy <hudaemad251@gmail.com>
2023-07-20 05:42:52 +0300

    D

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,14 +16,16 @@ void times_table(void)
 		{
 		_putchar(' ');
 		_putchar(',');
+
 		prod = num * mult;
+
 		if (prod <= 9)
 		{
 			_putchar(' ');
 		}
 		else
 			_putchar((prod / 10) + 48);
-		_putchar((prod / 10) + 48);
+		_putchar((prod % 10) + 48);
 	}
 _putchar('\n');
 }
